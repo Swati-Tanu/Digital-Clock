bc47a76837d1dda2cd9e7d25429d7244982165b8
Swati Tanu <swatitani01@gmail.com>
2025-04-24 10:31:18 +0530

    Bug Fix

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-let clock=function(){
+let clock = function () {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    var half = "";
-    if(hours>=12){
-        half="PM";
-    }else{
-        half="AM";
-    }
+  // Pad single digits with 0
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    document.querySelector("#hrs").innerText = hours;
-    document.querySelector("#mins").innerText = minutes;
-    document.querySelector("#secs").innerText = seconds;
-    document.querySelector("#ampm").innerText = half;
-    setInterval(clock,1000);
+  document.querySelector("#hrs").innerText = hours;
+  document.querySelector("#mins").innerText = minutes;
+  document.querySelector("#secs").innerText = seconds;
 };
 
+setInterval(clock, 1000);
 clock();
